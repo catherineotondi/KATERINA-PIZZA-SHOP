f7c7b35c23d5dc61a3158abf2d75fe08a9482473
catherineotondi <catherineotondi@gmail.com>
2022-03-20 16:33:50 +0300

    added the hide function in the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-   
+    $("table").hide();
+    $(".additional-buttons").hide();
+    $(".additional-info").hide();
+    $(".btn.yes").hide();
+    $(".btn.no").hide();
+    $(".additional-info h4").hide();
  });
